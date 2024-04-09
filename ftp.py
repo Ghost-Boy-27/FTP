3dbff645f792d60a8f24721efe1c933f8ddef23f
Ghost_Boy <129123736+Hacker273020@users.noreply.github.com>
2024-04-09 12:33:14 -0600

    ftp.py

--- ftp.py
+++ ftp.py
@@ -1,3 +1,9 @@
+# recuerden cambiar tu-ip por su ip real ejemplo 192.168.0.1
+# tambien cambien tu-usuario por el usuario de su laptop o computadora
+# tambien para que les funcione tienen que activar el Port Forwarding en la administracion de su Router
+# tambien hay que instalar la libreria necesaria con el siguiente comando 'pip install pyftpdlib'
+# para mas ayuda manda msg por instagram} ezemtz.2222
+
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
@@ -7,14 +13,14 @@ def main():
     authorizer = DummyAuthorizer()
 
     # Agrega un usuario con nombre 'admin' y contraseña 'password' con todos los permisos
-    authorizer.add_user("admin", "password", "C:/Users/lidia rodriguez/Desktop/", perm="elradfmw")
+    authorizer.add_user("admin", "password", "C:/Users/tu-usuario/Desktop/", perm="elradfmw")
 
     # Crea el manejador del servidor FTP
     handler = FTPHandler
     handler.authorizer = authorizer
 
     # Establece la dirección IP y el puerto del servidor FTP
-    address = ("192.168.0.177", 2121)
+    address = ("tu-ip", 2121)
 
     # Crea el servidor FTP con el manejador y la dirección especificada
     server = FTPServer(address, handler)
